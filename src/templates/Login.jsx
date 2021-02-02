c169f405ce7279102393878b717fd07b2cc3ac6f
wanko <kouji124@w4.dion.ne.jp>
2021-02-02 15:35:33 -0800

    fixed routing

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
+import { signInAction } from "../reducks/users/actions";
 
 const Login = () => {
   const dispatch = useDispatch();
   const selector = useSelector(state => state)
 
-  console.log(selector.router)
 
   return (
     <div>
       <h2>Login</h2>
-      <button onClick={() => dispatch(push('/'))}>
+      <button onClick={() => {
+          dispatch(signInAction({uid: "0001", username: "koji"}));
+          dispatch(push('/'))
+          }}>
           Login
       </button>
     </div>
