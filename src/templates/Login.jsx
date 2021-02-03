221145918c490526ac971509d5bbe5b73966c3c5
wanko <kouji124@w4.dion.ne.jp>
2021-02-02 16:08:30 -0800

    added thunk and operations.js

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { push } from "connected-react-router";
-import { signInAction } from "../reducks/users/actions";
+import { useDispatch } from "react-redux";
+import { signIn } from "../reducks/users/operations";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const selector = useSelector(state => state)
 
 
   return (
     <div>
       <h2>Login</h2>
-      <button onClick={() => {
-          dispatch(signInAction({uid: "0001", username: "koji"}));
-          dispatch(push('/'))
-          }}>
+      <button onClick={() => dispatch(signIn())}>
           Login
       </button>
     </div>
