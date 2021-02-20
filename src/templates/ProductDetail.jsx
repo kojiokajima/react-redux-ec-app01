699fdfd106196b464c33868be5c934aadcda4c53
wanko <kouji124@w4.dion.ne.jp>
2021-02-19 16:55:34 -0800

    added add-product-to-cart function

--- src/templates/ProductDetail.jsx
+++ src/templates/ProductDetail.jsx
@@ -1,9 +1,11 @@
 import { makeStyles } from '@material-ui/core'
 import React, { useState, useEffect, useCallback } from 'react'
-import { useSelector } from 'react-redux'
-import {db} from '../firebase/index'
+import { useDispatch, useSelector } from 'react-redux'
+import {db, FirebaseTimestamp} from '../firebase/index'
 import HTMLReactParser from 'html-react-parser'
 import {ImageSwiper, SizeTable} from '../components/Products'
+import {addProductToCart} from '../reducks/users/operations'
+
 
 
 
@@ -48,6 +50,7 @@ const returnCodeToBr = (text) => {
 
 const ProductDetail = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
     const selector = useSelector((state) => state)
     const path = selector.router.location.pathname
     const id = path.split('/product/')[1]
@@ -63,6 +66,21 @@ const ProductDetail = () => {
         })
     }, [])
 
+    const addProduct = useCallback((selectedSize) => {
+        const timeStamp =  FirebaseTimestamp.now()
+        dispatch(addProductToCart({
+            added_at: timeStamp,
+            description: product.description,
+            gender: product.gender,
+            images: product.images,
+            name: product.name,
+            price: product.price,
+            productId: product.id,
+            quantity: 1,
+            size: selectedSize
+        }))
+    }, [product])
+
     return (
         <section className="c-sectin-wrapin">
             {product && (
@@ -74,7 +92,7 @@ const ProductDetail = () => {
                         <h2 className="u-text__headline">{product.name}</h2>
                         <p className={classes.price}>{product.price.toLocaleString()}</p>
                         <div className="modul-space--small" />
-                        <SizeTable sizes={product.sizes} />
+                        <SizeTable addProduct={addProduct} sizes={product.sizes} />
                         <div className="modul-space--small" />
                         <p>{returnCodeToBr(product.description)}</p>
                     </div>
